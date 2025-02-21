85cfafc72a87a8f85998b5cb6409396544566426
Vitaly Kuzmin <vitalyqa34@gmail.com>
2025-02-21 15:17:01 +0300

    Добавил модели и спецификации в класс ApiExtendedTests

--- src/test/java/models/lombok/UserResponseDataModel.java
+++ src/test/java/models/lombok/UserResponseDataModel.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResponseDataModel {
 
     String  id, name, year, color, pantone_value;
